20261ee21ba7be183171c484c460f2b35267865b
wandi1209 <wandider@gmail.com>
2026-02-22 19:17:29 +0700

    Praktikum 3 : View

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,4 +9,10 @@ class WelcomeController extends Controller
     public function hello() {
         return 'Hello World';
     }
+
+    public function greeting(){
+        return view('blog.hello')
+            ->with('name','Andi')
+            ->with('occupation','Astronaut');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,4 @@ Route::get('/articles/{id}', [AboutController::class,'show']);
 
 Route::get('/hello', [WelcomeController::class,'hello']);
 
-Route::get('/world', function () {
-    return 'World';
-});
-
-Route::get('/user/{name}', function ($name) {
-    return 'Nama saya ' . $name;
-});
-
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
-
-Route::get('/user/{name?}', function ($name=null) {
-    return 'Nama saya '.$name;
-});
+Route::get('/greeting', [WelcomeController::class,'greeting']);
