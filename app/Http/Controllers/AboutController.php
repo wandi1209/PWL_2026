703799678c9d5953cab4f897bf6a6a8371020232
wandi1209 <wandider@gmail.com>
2026-02-22 16:32:14 +0700

    Praktikum 2 : Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function index(){
+        return 'Wandi - 244107020003';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PhotoController;
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+Route::resource('photos', PhotoController::class);
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class,'index']);
+Route::get('/articles/{id}', [AboutController::class,'show']);
+
+Route::get('/hello', [WelcomeController::class,'hello']);
 
 Route::get('/world', function () {
     return 'World';
-});
-
-Route::get('/about', function () {
-    return '244107020003 - Wandi';
 });
 
 Route::get('/user/{name}', function ($name) {
@@ -26,54 +28,6 @@ Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
 
-Route::get('/articles/{id}', function ($id) {
-    return 'Halaman Artikel dengan ID ' . $id;
-});
-
 Route::get('/user/{name?}', function ($name=null) {
     return 'Nama saya '.$name;
 });
-
-// ROUTE NAME
-Route::get('/user/profile', function () {})->name('profile');
-Route::get('/user/profile', [UserProfileController::class, 'show'])->name('profile');
-// Generating URLs...
-$url = route('profile');
-// Generating Redirects...
-return redirect()->route('profile');
-
-// Route Group
-Route::middleware(['first', 'second'])->group(function () {
-    Route::get('/', function () {
-        // Uses first & second middleware...
-    });
-    Route::get('/user/profile', function () {
-        // Uses first & second middleware...
-    });
-});
-
-Route::domain('{account}.example.com')->group(function () {
-    Route::get('user/{id}', function ($account, $id) {
-        //
-    });
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/user', [UserController::class, 'index']);
-    Route::get('/post', [PostController::class, 'index']);
-    Route::get('/event', [EventController::class, 'index']);
-});
-
-// Route Prefixes
-Route::prefix('admin')->group(function () {
-    Route::get('/user', [UserController::class, 'index']);
-    Route::get('/post', [PostController::class, 'index']);
-    Route::get('/event', [EventController::class, 'index']);
-});
-
-// Redirect Routes
-Route::redirect('/here', '/there');
-
-// View Routes
-Route::view('/welcome', 'welcome');
-Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
